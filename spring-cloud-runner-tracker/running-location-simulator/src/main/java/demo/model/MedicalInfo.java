1f63144a1fe06a4cae91f2a9ae8192ab83beedd0
starwavelin <benjaminxianlin@gmail.com>
2018-05-05 22:57:52 -0400

    added models for simulator

---
+++ spring-cloud-runner-tracker/running-location-simulator/src/main/java/demo/model/MedicalInfo.java
@@ -0,0 +1,14 @@
+package demo.model;
+
+import lombok.Data;
+
+@Data
+public class MedicalInfo {
+    private long medicalInfoId;
+    private String bandMake;
+    private String medicalInfoClassification;
+    private String description;
+    private String aidInstructions;
+
+    public MedicalInfo() {}
+}
