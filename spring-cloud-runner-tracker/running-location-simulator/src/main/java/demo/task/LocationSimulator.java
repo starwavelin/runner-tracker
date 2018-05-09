3a73c9ac28d653d3dee05adde46e82a47dfedad3
starwavelin <benjaminxianlin@gmail.com>
2018-05-09 15:17:00 -0400

    added constructor for LocationSimulator

--- spring-cloud-runner-tracker/running-location-simulator/src/main/java/demo/task/LocationSimulator.java
+++ spring-cloud-runner-tracker/running-location-simulator/src/main/java/demo/task/LocationSimulator.java
@@ -12,20 +12,34 @@ public class LocationSimulator implements  Runnable {
     private AtomicBoolean cancel = new AtomicBoolean();
 
     private double speedInMps;
-    private double shouldMove; // determine if a runner continues to run
+    private boolean shouldMove; // determine if a runner continues to run
     private boolean exportPositionsToMessaging = true;
     private int reportInterval = 500; // unit: ms
 
     private PositionInfo currentPosition = null;
 
     private List<Leg> legs;
-    private RunnerStatus runnerStatus = RunnerStatus.NONE;
     private String runningId;
+    private RunnerStatus runnerStatus = RunnerStatus.NONE;
 
     private Point startPoint;
     private Date executionStartTime;
 
     private MedicalInfo medicalInfo;
+
+    public LocationSimulator(GpsSimulatorRequest gpsSimulatorRequest) {
+        this.setSpeed(gpsSimulatorRequest.getSpeed());
+        this.shouldMove = gpsSimulatorRequest.isMove();
+        this.exportPositionsToMessaging = gpsSimulatorRequest.isExportPositionsToMessaging();
+        this.reportInterval = gpsSimulatorRequest.getReportInterval();
+        this.runningId = gpsSimulatorRequest.getRunningId();
+        this.runnerStatus = gpsSimulatorRequest.getRunnerStatus();
+        this.medicalInfo = gpsSimulatorRequest.getMedicalInfo();
+    }
+
+    public void setSpeed(double speed) {
+        this.speedInMps = speed;
+    }
 
     @Override
     public void run() {
